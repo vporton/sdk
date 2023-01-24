2bfc64286e9c15a5ef63d6e73e133d1cbfb5449a
Eric Swanson <eric.swanson@dfinity.org>
2023-01-24 00:45:31 -0800

    refactor: concrete error types for NetworkDescriptor, LocalServerDescriptor

--- src/dfx-core/src/error/network_config.rs
+++ src/dfx-core/src/error/network_config.rs
@@ -1,15 +1,25 @@
 use crate::error::io::IoError;
+use crate::error::socket_addr_conversion::SocketAddrConversionError;
 use std::num::ParseIntError;
 use std::path::PathBuf;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum NetworkConfigError {
-    #[error("Failed to parse : {0}")]
-    ParseProviderUrlFailed(Box<String>, url::ParseError),
+    #[error("Network '{0}' does not specify any network providers.")]
+    NetworkHasNoProviders(String),
+
+    #[error("The '{0}' network must be a local network.")]
+    NetworkMustBeLocal(String),
 
     #[error("Did not find any providers for network {0}")]
     NoProvidersForNetwork(String),
+
+    #[error("Failed to parse bind address: {0}")]
+    ParseBindAddressFailed(SocketAddrConversionError),
+
+    #[error("Failed to parse : {0}")]
+    ParseProviderUrlFailed(Box<String>, url::ParseError),
 
     #[error("Failed to parse contents of {0} as a port value")]
     ParsePortValueFailed(Box<PathBuf>, Box<ParseIntError>),
